44e4eb0ea4cf0d78f3744e9d7c0482fc13a96c73
ankitppatel07 <ankitppatel07@gmail.com>
2019-12-01 14:08:24 -0500

    "Added Request URI's"

--- BankApp/src/main/java/io/bankapp/BankAppApplication.java
+++ BankApp/src/main/java/io/bankapp/BankAppApplication.java
@@ -2,6 +2,11 @@ package io.bankapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * 
+ * @author Ankit Patel
+ *
+ */
 
 @SpringBootApplication
 public class BankAppApplication {
